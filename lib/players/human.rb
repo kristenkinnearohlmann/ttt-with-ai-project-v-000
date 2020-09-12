bfa2b8c18e92be6c9bfb5ce8b6da82b8338319f1
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-09-12 23:05:36 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,9 +1,15 @@
 module Players
   class Human < Player
+
     def move(board)
+
       puts "Select an open square:"
       input = gets.chomp
-      input
+      if !board.valid_move?
+        input = "invalid"
+      end
+
     end
+
   end
 end
