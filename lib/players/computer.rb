42eeb850488358a0f86f647162cac4421da9c4fa
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-09-12 15:04:24 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,8 +2,10 @@ module Players
   class Computer < Player
     def move(board)
       binding.pry
-      if @board[4] == " " #middle square is empty
+      if @board[4] == " " && @board.cells.count {|cell| cell == " "} == 9 #middle square is empty and game is new
         "5"
+      else
+
       end
     end
   end
