1bdc2b0b4713be6d48567f8e3887181e68e8660f
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-09-12 23:20:00 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -74,7 +74,7 @@ class Game
     position_req = "Select an open square"
     position_req += ", #{current_player.name}" if current_player.name != nil
     position_req += ":"
-    puts position_req
+    puts position_req if current_player.class == Players::Human
 
     while !over?
       board_display if current_player.class == Players:: Human
